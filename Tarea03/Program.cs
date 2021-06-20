6b86eeed134549c29f4a10ca1718aaa622f9cdca
Carlos <carlos@xtrene.com>
2021-06-20 10:42:07 +0200

    Agregar archivos de proyecto.

---
+++ Tarea03/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Tarea03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 12;
+            int b = 5;
+            Console.WriteLine("Valor de la variable a: {0}", a);
+            Console.WriteLine("Valor de la variable b: {0}", b);
+
+            Console.WriteLine("Suma: {0}",  a + b);
+            Console.WriteLine("Resta: {0}", a - b);
+            Console.WriteLine("Multiplicación: {0}", a * b);
+            Console.WriteLine("División: {0}", a / b);
+            Console.WriteLine("Módulo: {0}", a % b);
+            
+            Console.ReadKey();
+        }
+    }
+}
